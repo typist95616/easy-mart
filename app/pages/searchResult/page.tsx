fd513bbfc6cfeb338e2987f6bf87a7b9e88c0782
jackielai1205 <typist95616@gmail.com>
2025-05-18 10:43:53 +0800

    update searchResult page with Suspense

--- app/pages/searchResult/page.tsx
+++ app/pages/searchResult/page.tsx
@@ -4,11 +4,26 @@ import "./main.scss";
 import NavbarV2 from "../../components/NavBarV2";
 import FooterV2 from "../../components/FooterV2";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Product } from "@/app/types/Product";
 import ProductCardV2 from "../../components/SliderComponent/ProductCardV2";
 
 export default function Main() {
+
+    return (
+        <div>
+            <NavbarV2></NavbarV2>
+            <div className="searchResult-root">
+            <Suspense fallback={<div>Loading...</div>}>
+                <SearchResultContent></SearchResultContent>
+            </Suspense>
+            </div>
+            <FooterV2></FooterV2>
+        </div>
+    )
+}
+
+function SearchResultContent() {
 
     const searchParams = useSearchParams();
     const query = searchParams.get("query") || "";
@@ -28,21 +43,17 @@ export default function Main() {
     }, [query, filter]);
 
     return (
-        <div>
-            <NavbarV2></NavbarV2>
-            <div className="searchResult-root">
-                <div className="searchResult-header">
-                    <div className="searchResult-header-spacer"></div>
-                    <div className="searchResult-header-text">{query}</div>
-                </div>
-                <div className="searchResult-body">
-                    {products.length === 0 && <div>No Result...</div>}
-                    {products.map(product => (
-                        <ProductCardV2 productName={product.name} productImage={product.img_url} pricePerLb={product.price_per_lb} totalPrice={product.total_price} stock={product.stock} description={product.description} detail={product.detail}></ProductCardV2>
-                    ))}
-                </div>
+        <>
+            <div className="searchResult-header">
+                <div className="searchResult-header-spacer"></div>
+                <div className="searchResult-header-text">{query}</div>
             </div>
-            <FooterV2></FooterV2>
-        </div>
+            <div className="searchResult-body">
+                {products.length === 0 && <div>No Result...</div>}
+                {products.map(product => (
+                    <ProductCardV2 productName={product.name} productImage={product.img_url} pricePerLb={product.price_per_lb} totalPrice={product.total_price} stock={product.stock} description={product.description} detail={product.detail}></ProductCardV2>
+                ))}
+            </div>
+        </>
     )
 }
