5c68d1496ec760cf7796418e7bc33d38bff83970
jackielai1205 <typist95616@gmail.com>
2025-05-22 21:25:23 +0800

    completed emptypage for popup window

--- app/components/addressPageComponent/AddressInputPage.tsx
+++ app/components/addressPageComponent/AddressInputPage.tsx
@@ -1,4 +1,7 @@
 import "./AddressInputPage.scss"
+import Image from "next/image";
+import searchIcon from "../../../public/images/search.png";
+import { useState } from "react";
 
 interface AddressInputPageProps {
     setCurrentPage: (page: number) => void;
@@ -6,10 +9,30 @@ interface AddressInputPageProps {
 
 export default function AddressInputPage(props: AddressInputPageProps) {
 
+    const [searchActive, setSearchActive] = useState(false);
+
     return (
-        <div>
+        <div className="addressInputPage-root">
+            <div className="addressInputPage-addressSearchBar">
+                <Image src={searchIcon} alt="search-Icon" className="addressInputPage-addressSearchBar-searchIcon"></Image>
+                <input
+                    placeholder="Enter your address"
+                    className="addressInputPage-addressSearchBar-input"
+                    onFocus={() => setSearchActive(true)}
+                    onBlur={() => setSearchActive(false)}
+                >
+                </input>
+
+                {/* Search bar Dropdown menu */}
+                {searchActive && (
+                    <div className="addressInputPage-whole-dropdown">
+                        <div>Items</div>
+                    </div>
+                )}
+            </div>
+
             <div onClick={() => props.setCurrentPage(1)}>previous page</div>
             <div onClick={() => props.setCurrentPage(3)}>next page</div>
         </div>
-    )    
+    )
 }
--- app/pages/address/page.tsx
+++ app/pages/address/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+// "use client";
 
-import "./main.scss";
-import dynamic from "next/dynamic";
+// import "./main.scss";
+// import dynamic from "next/dynamic";
 
-const MapView = dynamic(() => import("../../components/addressPageComponent/Map"), { ssr: false })
+// const MapView = dynamic(() => import("../../components/addressPageComponent/Map"), { ssr: false })
 
-export default function AddressPage() {
-    return (
-        <div className="addressPage-root">
-            {/* <MapView></MapView> */}
-        </div>
-    )
-}
+// export default function AddressPage() {
+//     return (
+//         <div className="addressPage-root">
+//             <MapView></MapView>
+//         </div>
+//     )
+// }
