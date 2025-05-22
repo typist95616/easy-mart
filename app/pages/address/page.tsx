3fdf0386e9b94297f6aa983b278e8e306d42ba95
jackielai1205 <typist95616@gmail.com>
2025-05-22 21:27:18 +0800

    deploy again

--- app/pages/address/page.tsx
+++ app/pages/address/page.tsx
@@ -1,14 +1,15 @@
 // "use client";
 
-// import "./main.scss";
+import "./main.scss";
 // import dynamic from "next/dynamic";
 
 // const MapView = dynamic(() => import("../../components/addressPageComponent/Map"), { ssr: false })
 
-// export default function AddressPage() {
-//     return (
-//         <div className="addressPage-root">
-//             <MapView></MapView>
-//         </div>
-//     )
-// }
+export default function AddressPage() {
+    return (
+        <div className="addressPage-root">
+            <div>address page</div>
+            {/* <MapView></MapView> */}
+        </div>
+    )
+}
