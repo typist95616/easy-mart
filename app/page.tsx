ec2a0d62769d9d520c3c6cd4415040729d7d44a1
jackielai1205 <typist95616@gmail.com>
2025-07-26 16:56:42 +0800

    update main page path

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,88 @@
-import Image from "next/image";
+"use client";
+
+import FooterV2 from './components/FooterV2';
+import ProductSlider from "./components/SliderComponent/ProductSlider";
+import "./main.css";
+import NavbarV2 from '@/app/components/NavBarV2';
+import { useCurrentUser } from "./Context/CurrentUserContext";
+import { useAtom } from 'jotai';
+import { tokenAtom } from "./Context/TokenAtom";
+import ImageSlider from "./components/SliderComponent/StandaloneImageSlider";
+import ProductImageSlider from '@/app/components/ProductImageSlider';
+import { useEffect, useState } from 'react';
+import { Product } from '@/app/types/Product';
+
+const images = [
+    "../images/poster1.png",
+    "../images/poster2.png",
+    "../images/poster3.png",
+    "../images/poster4.png",
+    "../images/poster5.png",
+    "../images/poster1.png",
+    "../images/poster2.png",
+    "../images/poster3.png",
+    "../images/poster4.png",
+    "../images/poster5.png",
+]
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const { currentUser } = useCurrentUser();
+    console.log("User: ", JSON.stringify(currentUser, null, 2)); // Pretty print the user object
+    const [tokenValue, setToken] = useAtom(tokenAtom);
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [filter, setFilter] = useState([]);
+
+    // Get data from database
+    const fetchProducts = async () => {
+        const res = await fetch('/api/products');
+        const data = await res.json();
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [filter]);
+
+    return (
+        <div>
+            <NavbarV2 className="whole-navbar-v2" />
+            {/* {currentUser?.email} */}
+            {/* {tokenValue} */}
+            <div className='main-content'>
+                <div>
+                    <ImageSlider images={images} direction={"left"}></ImageSlider>
+                    <ImageSlider images={images} direction={"right"}></ImageSlider>
+                </div>
+                {/* <Banner /> */}
+                {/* <BestSeller Title="Best Seller"/> */}
+                {/* {/* <ProductSlider title="Best Seller"></ProductSlider> */}
+                {/* <ProductSlider title="Trending Store Favourites"></ProductSlider> */}
+                {/* <TrendingFavorites bgColor="whiteBackground"/>
+                <OrderNowYourGrocery />
+                <TrendingFavorites bgColor="greyBackground"/>
+                <OrderNow /> */}
+                <ProductImageSlider
+                    title="Best Seller"
+                    headingStyle={{
+                        fontSize: "20px",
+                    }}>
+                </ProductImageSlider>
+                <ProductImageSlider
+                    title="Trending Store Favorites"
+                    containerStyle={{
+                        border: "1px solid #F0EEF0",
+                        padding: "24px",
+                        borderRadius: "32px",
+                    }}
+                    headingStyle={{
+                        fontSize: "24px",
+
+                    }}>
+                </ProductImageSlider>
+            </div>
+            <FooterV2 />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
